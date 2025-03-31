9b2ae088fa2837ad70a3865028091e367d92e7a6
Steve Mafoa <steve_mafoa@icloud.com>
2025-03-31 11:17:41 +0200

    fix: fixed 'ajouter.php' inclusion with an absolute path

--- php_employee/index.php
+++ php_employee/index.php
@@ -1,13 +1,53 @@
-<?php
+<?php 
 
- // Ajoute les informations d'un employé dans un fichier texte
- function getInfos($nom, $prenom, $mail, $num) {
-    $handle = fopen("employee.txt", "a");
-    if ($handle) {
-        fwrite($handle, PHP_EOL.$nom."|".$prenom."|".$mail."|".$num);
-        fclose($handle);
+include("ajouter.php");
+
+$message = "";
+
+if($_SERVER['REQUEST_METHOD'] == 'POST') {
+    $nom = isset($_POST['nom']) ? trim($_POST['nom']) : "";
+    $prenom = isset($_POST['prenom']) ? trim($_POST['prenom']) : "";
+    $mail = isset($_POST['mail']) ? trim($_POST['mail']) : "";
+    $num = isset($_POST['num']) ? trim($_POST['num']) : "";
+
+
+    // Vérification des champs obligatoires
+    if (!empty($nom) && !empty($prenom) && !empty($mail) && !empty($num)) {
+        getInfos($nom, $prenom, $mail, $num);
+        $message = "Les informations ont été enregistrées avec succès.";
+    } else {
+        $message = "Veuillez remplir tous les champs.";
     }
+   
 }
 
+?>
 
-echo getInfos("Meek", "Mill", "test@test.fr", "0606060606");
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+<form method="post" action="">
+        <label for="nom">Nom :</label>
+        <input type="text" name="nom" id="nom" required>
+        <label for="prenom">Prenom :</label>
+        <input type="text" name="prenom" id="prenom" required>
+        <label for="email">Mail :</label>
+        <input type="email" name="mail" id="mail" required>
+        <label for="message">Numéro de tel :</label>
+        <input type="text" name="num" id="num" required>
+        <button type="submit">envoyer</button>
+
+        <p><?php echo $message; ?></p>
+    </form>
+</body>
+</html>
+
+
+
+
+
